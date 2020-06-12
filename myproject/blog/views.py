a25f9b5245cd9b9e7697e839815624b2da067826
edison0019 <edison_0019@hotmail.com>
2020-06-12 19:28:47 -0400

    finishing the python django introduction

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def req(request):
+    HttpResponse('hello world!')
+
+def information(request):
+    sex = request.GET['sex']
+    name = request.GET['name']
+    response = 'You are ' + name + ' and of sex ' + sex
+    HttpResponse(response)
 # Create your views here.
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from helloworld.views import hello
+from blog.views import information
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',hello)
+    path('',hello),
+    path('',information)
 ]
